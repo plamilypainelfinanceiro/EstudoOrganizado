bb2a485c2e18e2f4e2dfab4fa5c8f17adcf4d884
plamily <plamilypainelfinanceiro@gmail.com>
2026-08-05 10:05:57 -0400

    Delete sw.js

--- sw.js
+++
@@ -1,9 +0,0 @@
-const CACHE_NAME = "estudo-organizado-v1";
-
-self.addEventListener("install", (event) => {
-  self.skipWaiting();
-});
-
-self.addEventListener("fetch", (event) => {
-  event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
-});
